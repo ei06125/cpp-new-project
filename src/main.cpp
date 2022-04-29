22824fd035f9aa93b5527170dacd54eeb7a74539
Pedro André Oliveira <pedro.andre.oliveira@outlook.com>
2022-04-29 19:31:28 +0100

    WIP - Starts Doxygen Support

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include "StandardLibrary.hpp"
 
+/**
+ * @brief A test for the Doxygen Utility
+ *
+ */
+class DoxygenClass {
+  /**
+   * @brief a random value
+   *
+   */
+  int value;
+
+ public:
+  /**
+   * @brief Get the Value object
+   *
+   * @return int
+   */
+  int GetValue() { return value; }
+};
+
 int main() {
   std::cout << "Hello World!" << std::endl;
   return 0;
